8ea0ffb428dc10e226f9c5bb32e566bb7ca462f8
Kostyantyn Maltsev <maltsevk@gmail.com>
2022-08-17 13:07:23 +0200

    intendation

--- app/src/Redux/Actions/TwitterActions.jsx
+++ app/src/Redux/Actions/TwitterActions.jsx
@@ -1,9 +1,10 @@
 export const fetchPosts = () => (dispatch) => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(res => res.json())
-        .then(data => dispatch({
-            type: 'FETCH_POST',
-            payload: data
-        })
-        );
-}
+  fetch('https://jsonplaceholder.typicode.com/posts')
+    .then((res) => res.json())
+    .then((data) =>
+      dispatch({
+        type: 'FETCH_POST',
+        payload: data,
+      })
+    );
+};
--- app/src/Redux/Store/Store.jsx
+++ app/src/Redux/Store/Store.jsx
@@ -1,16 +1,14 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import RootReducer from '../Reducers/RootReducer'
+import RootReducer from '../Reducers/RootReducer';
 //redux dev tools for development purposes
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const initialState = {};
 const middleWare = [thunk];
 const store = createStore(
-    RootReducer,
-    initialState,
-    composeEnhancers(
-        applyMiddleware(...middleWare)
-    )
+  RootReducer,
+  initialState,
+  composeEnhancers(applyMiddleware(...middleWare))
 );
 
 export default store;
