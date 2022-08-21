47c2dd1c961c77ce815c1a3bfb171c1be8b87bd4
Kostyantyn Maltsev <maltsevk@gmail.com>
2022-08-21 15:37:21 +0200

    scroll bottom

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useGameInfo } from '../hooks/useGameInfo';
 import { useNavigate } from 'react-router-dom';
 import useHomeInfo from '../hooks/useHomeInfo';
+import styled from 'styled-components';
 
 function Home() {
     const [searchActive, setSearchActive] = useState(false);
@@ -31,7 +32,24 @@ function Home() {
         }
     }, [searchActive, searchAvailable, players, dispatchFilter]);
 
-    return <div></div>;
+    return (
+        <div>
+            {searchActive && (
+                <ScrollBottomButton onClick={dispatchScrollBottom}>
+                    SCROLL BOTTOM
+                </ScrollBottomButton>
+            )}
+        </div>
+    );
 }
 
 export default Home;
+
+const ScrollBottomButton = styled.button`
+    width: 100%;
+    /* margin: 20px; */
+    padding: 20px;
+    border: 0;
+    height: 400px;
+    font-size: 100px;
+`;
