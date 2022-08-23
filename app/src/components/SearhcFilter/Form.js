639524246fc1e7949791e89f67398b4819edfced
Kostyantyn Maltsev <maltsevk@gmail.com>
2022-08-23 17:12:55 +0200

    fix

--- app/src/components/SearhcFilter/Form.js
+++ app/src/components/SearhcFilter/Form.js
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState } from 'react';
 import { Form } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import styled from 'styled-components';
