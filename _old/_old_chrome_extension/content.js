3f1702a580f1b642e2ec010302e2d86bdf08dc7b
Kostyantyn Maltsev <maltsevk@gmail.com>
2022-09-01 01:30:15 +0200

    backup

---
+++ _old/_old_chrome_extension/content.js
@@ -0,0 +1,55 @@
+/* eslint-disable eqeqeq, no-undef */
+
+function n01rcu_scriptInjector(path) {
+    return new Promise((resolve, reject) => {
+        const script = document.createElement('script');
+
+        script.src = chrome.runtime.getURL(path);
+        script.onerror = () => reject();
+        script.onload = function () {
+            this.remove();
+            resolve();
+        };
+
+        (document.head || document.documentElement).appendChild(script);
+    });
+}
+
+function n01rcu_receiveEventsListener() {
+    if (
+        chrome == null ||
+        chrome.runtime == null ||
+        chrome.runtime.id == null
+    ) {
+        return;
+    }
+
+    chrome.runtime.onMessage.addListener(({ __type, ...data }) => {
+        if (__type === 'n01rcu.Event.Content') {
+            document.dispatchEvent(new CustomEvent(__type, { detail: data }));
+        }
+    });
+}
+
+function n01rcu_sendEventsListener(event) {
+    if (
+        chrome == null ||
+        chrome.runtime == null ||
+        chrome.runtime.id == null
+    ) {
+        return;
+    }
+
+    chrome.runtime.sendMessage({ __type: event.type, ...event.detail });
+}
+
+n01rcu_scriptInjector('content/helpers.js')
+    .then(() => n01rcu_scriptInjector('content/websocket.js'))
+    .then(() => n01rcu_scriptInjector('content/index.js'));
+
+// Events
+document.removeEventListener('n01rcu.Event.Background', n01rcu_sendEventsListener);
+document.addEventListener('n01rcu.Event.Background', n01rcu_sendEventsListener, false);
+document.removeEventListener('n01rcu.Event.Popup', n01rcu_sendEventsListener);
+document.addEventListener('n01rcu.Event.Popup', n01rcu_sendEventsListener, false);
+n01rcu_receiveEventsListener();
