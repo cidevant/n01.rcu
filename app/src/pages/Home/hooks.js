74342f81eef9043c1a4fd7d4d3ab73c3a3ce220e
User <user@example.com>
2022-10-02 10:28:51 +0200

    stats

--- app/src/pages/Home/hooks.js
+++ app/src/pages/Home/hooks.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import { useEffect, useMemo, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { config } from '../../config';
@@ -67,7 +68,7 @@ export function useStats() {
     }, [player]);
 
     useEffect(() => {
-        if (prevStatsId.current !== statsId) {
+        if (!_.isEmpty(statsId) && prevStatsId.current !== statsId) {
             fetchStats(statsId).then(dispatchSetStats);
 
             prevStatsId.current = statsId;
@@ -89,5 +90,50 @@ function fetchStats(id) {
 
                 return gameTime >= start.getTime() && gameTime <= end.getTime();
             })
-        );
+        )
+        .then((stats) => {
+            if (_.isArray(stats) && !_.isEmpty(stats)) {
+                return stats.reduce(
+                    (acc, game, index) => {
+                        const f9Score = (game.f9s / game.f9d) * 3;
+                        const score = (game.score / game.darts) * 3;
+
+                        acc.highFinish = Math.max(acc.highFinish, game.ho);
+                        acc.bestLeg = Math.min(acc.bestLeg, game.bst);
+                        acc.worstLeg = Math.max(acc.worstLeg, game.wst);
+                        acc['100'] += game.t00;
+                        acc['140'] += game.t40;
+                        acc['180'] += game.t80;
+
+                        acc.sets.win += Math.round(game.leg / 2) >= game.win;
+                        acc.sets.total += 1;
+                        acc.legs.win += game.win;
+                        acc.legs.total += game.leg;
+
+                        return acc;
+                    },
+                    {
+                        date: Date.now(),
+                        sets: {
+                            win: 0,
+                            total: 0,
+                        },
+                        legs: {
+                            win: 0,
+                            total: 0,
+                        },
+                        100: 0,
+                        140: 0,
+                        180: 0,
+                        highFinish: 0,
+                        bestLeg: 0,
+                        worstLeg: 0,
+                        score: 0,
+                        f9Score: 0,
+                    }
+                );
+            }
+
+            return null;
+        });
 }
