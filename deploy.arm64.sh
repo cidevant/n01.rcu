c978042e0848afe0da2056635548ca0f635f6217
User <user@example.com>
2022-11-27 10:34:20 +0100

    xxx

--- deploy.arm64.sh
+++ deploy.arm64.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 
-# build image on local machine
+
+echo ''
+echo '======== BUILD IMAGE'
 docker rmi registry.docker.devant.cz/arm64/n01rcu:latest
 docker build -t registry.docker.devant.cz/arm64/n01rcu:latest --platform linux/arm64 .
 docker push registry.docker.devant.cz/arm64/n01rcu:latest
 
-# upload docker-compose
+
+echo ''
+echo '======== UPDATE DOCKER COMPOSER FILE'
 scp ./docker-compose.arm64.prod.yml nemo@devant.cz:/var/www/n01.devant.cz/docker-compose.yml
 
-# update image from repository on target machine and start docker composer
+
+echo ''
+echo '======== UPDATE IMAGE AND RUN DOCKER COMPOSER'
 ssh nemo@devant.cz 'cd /var/www/n01.devant.cz && sudo docker pull registry.docker.devant.cz/arm64/n01rcu:latest && sudo docker compose up -d'
