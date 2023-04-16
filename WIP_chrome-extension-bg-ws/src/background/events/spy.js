646133284a051d647a7883f5cad6dcc090e587a9
Cidevant Von Goethe <cidevant@mail.ru>
2023-04-16 12:29:27 +0200

    finalization

--- WIP_chrome-extension-bg-ws/src/background/events/spy.js
+++ WIP_chrome-extension-bg-ws/src/background/events/spy.js
@@ -11,56 +11,38 @@ async function n01rcu$background$spyMessagesHandler(event, port) {
         case $SHARED.actions.SPY_LOADED:
             await $SHARED_BACKGROUND.dispatchToSpy({ type: $SHARED.actions.GET_DATA });
             break;
+        case $SHARED.actions.SPY_UNLOAD:
+            {
+                // const reason = $BACKGROUND_WEBSOCKET.closeErrors['4051'];
+                // webSocketDisconnect({
+                //     code: reason?.code,
+                //     reason: reason?.text,
+                // });
+            }
+            break;
         case $SHARED.actions.SET_DATA:
             await $SHARED_STORAGE.saveData(payload);
             await $SHARED_BACKGROUND.dispatchToSpy({
                 type: $SHARED.actions.WATCH_NATIVE_FUNCTIONS,
             });
             break;
-        case $SHARED.actions.SET_ICON:
-            $SHARED_HELPERS.changeChromeExtensionIcon(payload);
-            break;
-        case $SHARED.actions.WEBSOCKET_DISCONNECT:
-            n01rcu$background$webSocketDisconnect(payload);
-            break;
         case $SHARED.actions.WEBSOCKET_SEND:
-            n01rcu$background$webSocketSend(payload);
+            {
+                let newPayload = null;
+
+                try {
+                    newPayload = payload?.length > 0 ? JSON.parse(payload) : null;
+                } catch (error) {
+                    $$DEBUG &&
+                        $$VERBOSE &&
+                        console.log(
+                            "[n01.RCU.content.events] cant parse 'payload' for WEBSOCKET_SEND",
+                            event
+                        );
+                }
+
+                n01rcu$background$webSocketSend(newPayload);
+            }
             break;
     }
 }
-// function contentForegroundEventsHandler(event) {
-//     $$DEBUG && $$VERBOSE && console.log('[n01.RCU.content.events] got event from FG', event);
-
-//     const { type, payload } = event;
-
-//     switch (type) {
-//         case $SHARED.actions.SPY_UNLOAD: {
-//             const reason = $SHARED_WEBSOCKET.closeErrors['4051'];
-
-//             webSocketDisconnect({
-//                 code: reason?.code,
-//                 reason: reason?.text,
-//             });
-
-//             break;
-//         }
-//         case $SHARED.actions.WEBSOCKET_SEND: {
-//             let newPayload = null;
-
-//             try {
-//                 newPayload = payload?.length > 0 ? JSON.parse(payload) : null;
-//             } catch (error) {
-//                 $$DEBUG &&
-//                     $$VERBOSE &&
-//                     console.log(
-//                         "[n01.RCU.content.events] cant parse 'payload' for WEBSOCKET_SEND",
-//                         event
-//                     );
-//             }
-
-//             webSocketSend(newPayload);
-
-//             break;
-//         }
-//     }
-// }
--- WIP_chrome-extension-bg-ws/src/background/ws/events.js
+++ WIP_chrome-extension-bg-ws/src/background/ws/events.js
@@ -7,14 +7,17 @@ async function n01rcu$background$webSocketMessagesHandler(message) {
     // Update UI when `PAIRED` state changed
     switch (message?.type) {
         case 'PAIRED':
-        case 'UNPAIRED': {
-            const paired = message?.type === 'PAIRED';
+        case 'UNPAIRED':
+            {
+                const paired = message?.type === 'PAIRED';
 
-            await $SHARED_STORAGE.updateConnection({ paired });
-            await $SHARED_BACKGROUND.dispatchToPopup({ type: $SHARED.actions.UPDATE });
-            $SHARED_HELPERS.changeChromeExtensionIcon({ icon: paired ? 'paired' : 'connected' });
+                await $SHARED_STORAGE.updateConnection({ paired });
+                await $SHARED_BACKGROUND.dispatchToPopup({ type: $SHARED.actions.UPDATE });
+                $SHARED_HELPERS.changeChromeExtensionIcon({
+                    icon: paired ? 'paired' : 'connected',
+                });
+            }
             break;
-        }
     }
 
     // Forward message to `SPY`
